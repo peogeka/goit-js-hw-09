7c3dc5a2901c4ce756fd4a7afeb5a390d8fa9ead
peogeka <peo.geka@gmail.com>
2023-07-22 11:04:05 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = { position, delay };
+
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const firstDelay = Number(formData.get('delay'));
+    const step = Number(formData.get('step'));
+    const amount = Number(formData.get('amount'));
+
+    for (let i = 0; i < amount; i++) {
+      const currentDelay = firstDelay + i * step;
+
+      createPromise(i + 1, currentDelay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  });
+});
