9b51dad345177d17be2733a77fdd45d13a66c2aa
peogeka <peo.geka@gmail.com>
2023-07-23 10:02:17 +0200

    end

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,34 +1,29 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
 
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const result = { position, delay };
 
-      if (shouldResolve) {
-        resolve(result);
-      } else {
-        reject(result);
-      }
-    }, delay);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.querySelector('.form');
-
-  form.addEventListener('submit', function(event) {
+  document.querySelector(".form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const formData = new FormData(form);
-    const firstDelay = Number(formData.get('delay'));
-    const step = Number(formData.get('step'));
-    const amount = Number(formData.get('amount'));
+    const formData = new FormData(event.target);
+    const firstDelay = parseInt(formData.get("delay"));
+    const delayStep = parseInt(formData.get("step"));
+    const amount = parseInt(formData.get("amount"));
 
-    for (let i = 0; i < amount; i++) {
-      const currentDelay = firstDelay + i * step;
-
-      createPromise(i + 1, currentDelay)
+  
+    for (let i = 1; i <= amount; i++) {
+      const currentDelay = firstDelay + (i - 1) * delayStep;
+      createPromise(i, currentDelay)
         .then(({ position, delay }) => {
           console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
         })
@@ -37,4 +32,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
   });
-});
